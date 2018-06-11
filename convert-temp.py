651029d2fd2ec9f1cd2ceadefcdd5b3f7aae9428
aaron.hackney <aaron.hackney@rackspace.com>
2018-06-10 23:11:14 -0400

    milestone

--- convert-temp.py
+++ convert-temp.py
@@ -4,19 +4,19 @@ from tkinter import ttk
 
 
 class ConvertTemp(tk.Frame):
-    def __init__(self, parent):
-        tk.Frame.__init__(self, parent)
+    def __init__(self, title, master=None):
+        tk.Frame.__init__(self, master)
+        self.grid()
+        self.master.title(title)
         self.celsius = tk.DoubleVar()
         self.celsius.set(0.0)
         self.fahrenheit = tk.DoubleVar()
         self.fahrenheit.set(32.0)
-        self.window = None          # Main content Frame
         self.f_entry = None
         self.f_btn = None
         self.c_entry = None
         self.c_btn = None
         self.selection = tk.StringVar()
-        #self.build_gui_frame()
         self._add_widgets()
 
     def f_to_c(self):
@@ -30,15 +30,6 @@ class ConvertTemp(tk.Frame):
             self.fahrenheit.set((self.celsius.get() * 1.8) + 32)
         except ValueError:
             print("ERROR")
-
-    def build_gui_frame(self):
-        # self.tk_root.title("Temperature Conversion")
-        # self._init_variables()
-        # self.window = ttk.Frame(self.tk_root, padding="12 12 12 12")                 # window setup
-        self.window.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))   # Define the grid
-        self.window.columnconfigure(0, weight=1)                             # adjust window size
-        self.window.rowconfigure(0, weight=1)                                # automatically
-        self._add_widgets()
 
     def clear_widget(self, event):
         event.widget.delete(0, tk.END)
@@ -66,27 +57,27 @@ class ConvertTemp(tk.Frame):
         # This point to the input validation method
         vcmd = (self.register(self.onValidate), '%d', '%i', '%P', '%s', '%S', '%v', '%V', '%W')
 
-        self.radio1 = ttk.Radiobutton(root, text="F\xb0 to C\xb0", variable=self.selection, value=1, command=self.radio_select)
+        self.radio1 = ttk.Radiobutton(self, text="F\xb0 to C\xb0", variable=self.selection, value=1, command=self.radio_select)
         self.radio1.grid(row=0, column=0)
-        self.radio2 = ttk.Radiobutton(root, text="C\xb0 to F\xb0", variable=self.selection, value=2, command=self.radio_select)
+        self.radio2 = ttk.Radiobutton(self, text="C\xb0 to F\xb0", variable=self.selection, value=2, command=self.radio_select)
         self.radio2.grid(row=1, column=0)
 
         # Create the input fields and wire the associated events
-        ttk.Label(self.window, text="Fahrenheit\xb0").grid(row=0, column=1, sticky=tk.E, padx=5)
-        self.f_entry = ttk.Entry(self.window, width=5, validate="all", textvariable=self.fahrenheit, validatecommand=vcmd)
+        ttk.Label(self, text="Fahrenheit\xb0").grid(row=0, column=1, sticky=tk.E, padx=5)
+        self.f_entry = ttk.Entry(self, width=5, validate="all", textvariable=self.fahrenheit, validatecommand=vcmd)
         self.f_entry.bind("<FocusIn>", self.clear_widget)
         self.f_entry.grid(row=0, column=2, sticky=tk.W)
 
-        ttk.Label(self.window, text="Celsius\xb0").grid(row=1, column=1, sticky=tk.E, padx=5)
-        self.c_entry = ttk.Entry(self.window, width=5, textvariable=self.celsius)
+        ttk.Label(self, text="Celsius\xb0").grid(row=1, column=1, sticky=tk.E, padx=5)
+        self.c_entry = ttk.Entry(self, width=5, validate="all", textvariable=self.celsius, validatecommand=vcmd)
         self.c_entry.bind("<FocusIn>", self.clear_widget)
         self.c_entry.grid(row=1, column=2, sticky=tk.W)
 
         # Add buttons and wire them to functions
-        self.f_btn = tk.Button(self.window, text="Convert Fº to Cº", command=lambda: self.f_to_c())
+        self.f_btn = tk.Button(self, text="Convert Fº to Cº", command=lambda: self.f_to_c())
         self.f_btn.grid(row=2, column=0, columnspan=3, sticky=tk.W + tk.E)
 
-        self.c_btn = tk.Button(self.window, text="Convert Cº to Fº", command=lambda: self.c_to_f())
+        self.c_btn = tk.Button(self, text="Convert Cº to Fº", command=lambda: self.c_to_f())
         self.c_btn.grid(row=2, column=0, columnspan=3, sticky=tk.W + tk.E)
 
         # Select F-->C First!
@@ -109,6 +100,5 @@ class ConvertTemp(tk.Frame):
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    elements = ConvertTemp(root)
+    root = ConvertTemp('Temp Conversion')
     root.mainloop()
